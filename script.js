73b7abc7e39f4aed9a1251482889642187617965
Shawanda Ford <shawanda.ford@gmail.com>
2020-08-26 12:34:38 -0400

    updated overflow css on trail page and added map

--- script.js
+++ script.js
@@ -3,34 +3,61 @@
 //         options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
 //     }
 // });
+//global scope variables
 var trailsURL;
 var lon;
 var lat;
 var city;
 var trailsLength;
 
-$(".currentLocation").on("click", function () {
-    event.stopPropagation();
+//new
 
+
+/// geolocation access
+$("#currentLocation").on("click", function () {
+    event.stopPropagation();
+    //if they accept to share their location then run showPosition
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
+        
+ 
+    
 
     } else {
         alert("Geolocation is not supported by this browser.");
     }
 })
+// once permited we can access to the location to get the lat and lon
+// pass this parameters to trailsURL 
 function showPosition(position) {
     lat = position.coords.latitude.toFixed(4);
     lon = position.coords.longitude.toFixed(4);
     trailsURL = `https://www.hikingproject.com:443/data/get-trails?lat=${lat}&lon=${lon}&key=200880336-4b1739fed679fe7233ad0872e74e7fcd`
     console.log(trailsURL)
+
+    
+    var map= L.map('mapid')
+    map.setView([lat, lon],13)
+    L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1Ijoic2ZvcmQ0MTg2IiwiYSI6ImNrZWJsYmR2aTAwOWgycXF0Z2luYW5yYWYifQ.8ySHyDgtKaAj9wJU_AZV4A'
+}).addTo(map);
+var marker=L.marker([latitude, longitude]).addTo(map);
+marker.bindPopup("<b>You ARe HEre</b>").openPopup();
+
     renderTrails();
+    
 
 }
 
+
+// 
 $('#submit').click(function () {
     city = $("#search-bar").val();
-    console.log(city);
     var weatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=420fa54141903a76b9ac423622e9920d`
     $.ajax({
         url: weatherURL,
@@ -41,51 +68,115 @@ $('#submit').click(function () {
         lon = response.coord.lon
         console.log(lat)
         console.log(lon)
-
         trailsURL = `https://www.hikingproject.com:443/data/get-trails?lat=${lat}&lon=${lon}&key=200880336-4b1739fed679fe7233ad0872e74e7fcd`
         console.log(trailsURL)
         renderTrails();
 
     })
+    
 })
 //})
-
+function createAside (){
+    
+}
 function renderTrails() {
+    createAside();
+    $.ajax({
+        url: trailsURL,
+        method: "GET"
+
+    }).then(function (response) {
+        $(".splash-container").empty();
+        console.log(response)
+        $(".splash-container").append(`<section id= "displayBox" class="container"></section>`)
+        $("#displayBox").append(`<div id= "displayBox1"class="row"></div>`)
+        let x = 1
+        for (i = 0; i < response.trails.length - 1; i++) {
+            if (i===3){
+                $("#displayBox").append(`<div id= "displayBox2"class="row"></div>`)
+                x=2
+            }
+            if (i===6){
+                $("#displayBox").append(`<div id= "displayBox3"class="row"></div>`)
+                x=3
+            }
+
+            $(`#displayBox${x}`).append(`<div id = "displayAll${i}"></div>`)
+            $(`#displayAll${i}`).attr("class", "card col")
+            $(`#displayAll${i}`).attr("data-position", i)
+           // NAME 
+           var trailname =  $("<h4> ")
+            $(trailname).html(response.trails[i].name)
+            $(`#displayAll${i}`).append(trailname)
+            // IMG SM SQ
+            var trailimage = $("<img>")     
+            $(trailimage).attr("src", response.trails[i].imgSqSmall)
+            $(trailimage).attr("class", "images")
+            $(`#displayAll${i}`).append(trailimage)
+            // LENGTH
+            var trailLength =  $("<p> ")
+            $(trailLength).html(response.trails[i].length + "miles")
+            $(`#displayAll${i}`).append(trailLength)
+
+          //  $(`#displayAll${i}`).append(`<p> Length:  ${response.trails[i].length} milles </p>`)
+            // SUMMARY
+            var trailsummary =  $("<i> ")
+            $(trailsummary).html(response.trails[i].summary)
+            $(`#displayAll${i}`).append(trailsummary)
+
+          //$(`#displayAll${i}`).append(`<p> ${response.trails[i].summary} </p>`)
+
+        }
+        
+    })
+} 
+
+function eachtrail() {
 
     $.ajax({
         url: trailsURL,
         method: "GET"
 
     }).then(function (response) {
-        console.log(response)
         trailsLength = response.trails.length;
         $(".displayBox").empty();
+        console.log(response)
         
-        for (i = 0; i < trailsLength; i++) {
-            $(".displayBox").append(`<div id = "displayAll${i}"></div>`)
-            $(`#displayAll${i}`).attr("class", "card")
-
-            var trailname = $("<h4> ")
+            $(".displayBox").append(`<div id = 'display'></div>`)
+            $('#display').attr("class", "card")
+            // NAME
+           var trailname =  $("<h4> ")
             $(trailname).html(response.trails[i].name)
-            $(`#displayAll${i}`).append(trailname)
-
-            var trailimage = $("<img>")
-            $(trailimage).attr("src", response.trails[i].imgSqSmall)
+            $(`#display`).append(trailname)
+            //RATTINGS
+            var trailratting  =  $("<p> ")
+            $(trailratting).html(response.trails[i].difficulty)
+            $(`#display`).append(trailratting)
+            // IMG MED 
+            var trailimage = $("<img>")     
+            $(trailimage).attr("src", response.trails[i].imgMedium)
             $(trailimage).attr("class", "images")
-            $(`#displayAll${i}`).append(trailimage)
-
-            var trailLength = $("<p> ")
+            $(`#display`).append(trailimage)
+            // DIFFICULTY
+            var traildifficulty =  $("<p> ")
+            $(traildifficulty).html(response.trails[i].difficulty)
+            $(`#display`).append(traildifficulty)
+            // ACSENDING
+            var trailascending =  $("<p> ")
+            $(trailascending).html(response.trails[i].ascent)
+            $(`#display`).append(trailascending)
+            // DESCENDING
+            var traildescending =  $("<p> ")
+            $(traildescending).html(response.trails[i].descent)
+            $(`#display`).append(traildescending)
+            // LENGTH
+            var trailLength =  $("<p> ")
             $(trailLength).html(response.trails[i].length + "miles")
-            $(`#displayAll${i}`).append(trailLength)
-
-            //  $(`#displayAll${i}`).append(`<p> Length:  ${response.trails[i].length} milles </p>`)
-
-            var trailsummary = $("<i> ")
+            $(`#display`).append(trailLength)
+            // SUMMARY
+            var trailsummary =  $("<i> ")
             $(trailsummary).html(response.trails[i].summary)
-            $(`#displayAll${i}`).append(trailsummary)
+            $(`#display`).append(trailsummary)
 
-            //$(`#displayAll${i}`).append(`<p> ${response.trails[i].summary} </p>`)
-
-        }
     })
-} 
+}
